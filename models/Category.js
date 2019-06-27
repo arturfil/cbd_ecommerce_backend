1f2172a714d1b16fc3f28071aa686ac212816ef1
Arturo Filio Villa <arturfil@hotmail.com>
2019-06-27 02:31:39 -0400

    changed the category model to be unique

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,8 @@ const categorySchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlenght: 32
+      maxlenght: 32,
+      unique: true
     }
   },
   {timestamps: true}
